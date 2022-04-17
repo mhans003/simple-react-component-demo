3c85da3284a4aa691c5163a8b5b99de0ed97668b
mhans003 <michaeledwardhanson@gmail.com>
2022-04-17 07:11:56 -0700

    Reset values after submission and validate inputs before resetting

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -27,18 +27,32 @@ const AddUser = props => {
     //Handle what happens when user submits username and age
     const addUserHandler = event => {
         event.preventDefault();
+
+        //Validate inputs
+        if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            return;
+        }
+        if(+enteredAge < 1) {
+            return;
+        }
+
+        //Demo output in console
         console.log(enteredUsername);
         console.log(enteredAge);
+
+        //Reset state values
+        setEnteredUsername('');
+        setEnteredAge('');
     };
 
     return (
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
-                <input id="username" type="text" onChange={usernameChangeHandler}/>
+                <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler}/>
 
                 <label htmlFor="age">Age</label>
-                <input id="age" type="number" onChange={ageChangeHandler}/>
+                <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}/>
 
                 <Button type="submit">Add User</Button>
             </form>
